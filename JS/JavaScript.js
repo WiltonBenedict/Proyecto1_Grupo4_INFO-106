5d898e2a1fd88bab8fdc0c371c79ff494c742a01
Wilton <wilton.benedict0354@uhispano.ac.cr>
2024-03-18 22:24:39 -0600

    Update

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -15,3 +15,15 @@ $('#reloj2').countdown('2024/03/26', function(event) {
 $('#reloj3').countdown('2024/03/29', function(event) {
   $(this).html(event.strftime('%D días %H horas %M minutos'));
 });
+
+function toggleContent() {
+  // Obtiene el elemento div que contiene los párrafos
+  var content = document.getElementById("toggleContent");
+  
+  // Comprueba si el contenido está visible y alterna su estado
+  if (content.style.display === "none") {
+      content.style.display = "block";
+  } else {
+      content.style.display = "none";
+  }
+}
